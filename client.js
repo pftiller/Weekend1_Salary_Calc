93e16a27a9e212e685d6427043ec0822384ca274
pftiller <pftiller@gmail.com>
2017-12-17 21:48:58 -0600

    Base and Hard modes

---
+++ client.js
@@ -0,0 +1,28 @@
+function Employee(first_name, last_name, id_number, job_title, annual_salary) {
+  function monthly_pay () {
+    return annual_salary / 12;
+  }
+  this.first_name = first_name;
+  this.last_name = last_name;
+  this.id_number = id_number;
+  this.job_title = job_title;
+  this.annual_salary = annual_salary;
+  this.monthly_pay = monthly_pay();
+}
+$(document).ready(function(){
+
+
+//event listener
+$('#submit').on('click', addEmployee);
+$('#employee-list').on('click', '.delete', deleteEmployee);
+});
+
+function addEmployee() {
+    var employee = new Employee ($('#first_name').val(),$('#last_name').val(),$('#id_number').val(),$('#job_title').val(),$('#annual_salary').val());
+    $('#employees').append('<tr><td>' + employee.first_name + '</td><td>' + employee.last_name + '</td><td>' + employee.id_number + '</td><td>' + employee.job_title + '</td><td>' + employee.annual_salary + '</td><td>' + Math.round(employee.monthly_pay) + '</td><td><button class="delete">Delete</button></td></tr>');
+    $('input').val('');
+}
+function deleteEmployee() {
+  console.log('Delete this')
+  $(this).parent().parent().remove();
+}
