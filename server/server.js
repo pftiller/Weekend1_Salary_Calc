fe7265066120d6d32338937d481eaddc46b84798
pftiller <pftiller@gmail.com>
2018-01-15 16:27:48 -0600

    Running fine on local

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-/Users/paultiller/prime/assignments/week-one/Weekend1_Salary_Calc/server/public/scripts/client.js
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.use(express.static('./server/public'));
+
+app.listen(port, () => {
+  console.log('Now listening on port: ', port);
+});
